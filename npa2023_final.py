e18fa3fc3121324c2a5ca786c135835dd84ee49d
Iwachax <somo.osossomo@gmail.com>
2024-03-22 14:58:54 +0700

    task1-4

--- npa2023_final.py
+++ npa2023_final.py
@@ -1,24 +1,27 @@
 #######################################################################################
-# Yourname:
-# Your student ID:
-# Your GitHub Repo: 
+# Yourname: Wanichabhorn Chaisupasin
+# Your student ID: 64070226
+# Your GitHub Repo: https://github.com/Iwachax/NPA2023-Final-Wanichabhorn
 
 #######################################################################################
 # 1. Import libraries for API requests, JSON formatting, time, and (restconf_final or netconf_final).
 
-<!!!REPLACEME with code for libraries>
+import json
+import requests
+import time
+import restconf_final
 
 #######################################################################################
 # 2. Assign the Webex hard-coded access token to the variable accessToken.
 
-accessToken = "Bearer <!!!REPLACEME with hard-coded token!!!>"
+accessToken = "Bearer MmJkYTI3OGYtNTY2ZC00YmVhLTg0MTctY2QwODdlNjM0ODhiOTdlMWI4MjAtZTg2_P0A1_19365916-598f-457d-9ca7-51a422c8769e"
 
 #######################################################################################
 # 3. Prepare parameters get the latest message for messages API.
 
 # Defines a variable that will hold the roomId
 roomIdToGetMessages = (
-    "<!!!REPLACEME with roomID of the NPA2023 Webex Teams room!!!>"
+    "Y2lzY29zcGFyazovL3VybjpURUFNOnVzLXdlc3QtMl9yL1JPT00vYWZkZWUyMzAtZGM3OC0xMWVlLTk4MTktZTFjN2ZmNmY0MGMw"
 )
 
 while True:
@@ -31,18 +34,17 @@ while True:
     getParameters = {"roomId": roomIdToGetMessages, "max": 1}
 
     # the Webex Teams HTTP header, including the Authoriztion
-    getHTTPHeader = {"Authorization": <!!!REPLACEME!!!>}
+    getHTTPHeader = {"Authorization": accessToken}
 
 # 4. Provide the URL to the Webex Teams messages API, and extract location from the received message.
     
     # Send a GET request to the Webex Teams messages API.
     # - Use the GetParameters to get only the latest message.
     # - Store the message in the "r" variable.
-    r = requests.get(
-        "<!!!REPLACEME with URL of Webex Teams Messages API!!!>",
-        params=<!!!REPLACEME with HTTP parameters!!!>,
-        headers=<!!!REPLACEME with HTTP headers!!!>,
-    )
+    r = requests.get("https://webexapis.com/v1/messages",
+                         params = getParameter, 
+                         headers = {"Authorization": accessToken}
+                    )
     # verify if the retuned HTTP status code is 200/OK
     if not r.status_code == 200:
         raise Exception(
@@ -65,16 +67,17 @@ while True:
 
     # check if the text of the message starts with the magic character "/" followed by your studentID and a space and followed by a command name
     #  e.g.  "/66070123 create"
-    if message.find("<!!!REPLACEME!!!>") == 0:
+    if message.find("/") == 0:
+        space =  message.find(" ")
 
         # extract the command
-        command = <!!!REPLACEME!!!>
+        command = message[space+1:]
         print(command)
 
 # 5. Complete the logic for each command
 
         if command == "create":
-            <!!!REPLACEME with code for create command!!!>     
+            print("Interface loopback " +message+ "is created successfully")     
         elif command == "delete":
             <!!!REPLACEME with code for delete command!!!>
         elif command == "enable":
@@ -89,18 +92,18 @@ while True:
 # 6. Complete the code to post the message to the Webex Teams room.
         
         # the Webex Teams HTTP headers, including the Authoriztion and Content-Type
-        postHTTPHeaders = HTTPHeaders = {"Authorization": <!!!REPLACEME!!!>, "Content-Type": <!!!REPLACEME!!!>}
+        postHTTPHeaders = HTTPHeaders = {"Authorization": accessToken, "Content-Type": application/json}
 
         # The Webex Teams POST JSON data
         # - "roomId" is is ID of the selected room
         # - "text": is the responseMessage assembled above
-        postData = {"roomId": <!!!REPLACEME!!!>, "text": <!!!REPLACEME!!!>}
+        postData = {"roomId": roomIdToGetMessages, "text": responseMessage}
 
         # Post the call to the Webex Teams message API.
         r = requests.post(
-            "<!!!REPLACEME with URL of Webex Teams Messages API!!!>",
-            data=json.dumps(<!!!REPLACEME!!!>a),
-            headers=<!!!REPLACEME!!!>,
+            "https://webexapis.com/v1/messages",
+            data=json.dumps(postData),
+            headers= HTTPHeaders,
         )
         if not r.status_code == 200:
             raise Exception(
--- restconf_final.py
+++ restconf_final.py
@@ -3,30 +3,44 @@ import requests
 requests.packages.urllib3.disable_warnings()
 
 # Router IP Address is 10.0.15.189
-api_url = "<!!!REPLACEME with URL of RESTCONF Configuration API!!!>"
+#api_url = "https://10.0.15.189/restconf/data/ietf-interfaces:interfaces"
+api_url = "https://10.0.15.189/restconf/data/ietf-interfaces:interfaces/interface=Loopback"
 
 # the RESTCONF HTTP headers, including the Accept and Content-Type
 # Two YANG data formats (JSON and XML) work with RESTCONF 
-headers = <!!!REPLACEME with Accept and Content-Type information headers!!!>
+headers = { "Accept": "application/yang-data+json", 
+            "Content-type":"application/yang-data+json"
+           }
 basicauth = ("admin", "cisco")
 
 
 def create():
-    yangConfig = <!!!REPLACEME with YANG data!!!> 
+    yangConfig = {
+    "ietf-interfaces:interface": {
+        "name": "LoopbackStudentID",
+        "description": "StudentID RESTCONF loopback",
+        "type": "iana-if-type:softwareLoopback",
+        "enabled": True,
+        "ietf-ip:ipv4": {
+            "address": [
+                {
+                    "ip": "10.0.15.189",
+                    "netmask": "255.255.255.0"
+                }
+            ]
+        },
+        "ietf-ip:ipv6": {}
+    }
+}
 
-    resp = requests.<!!!REPLACEME with the proper HTTP Method!!!>(
-        <!!!REPLACEME with URL!!!>, 
-        data=json.dumps(<!!!REPLACEME with yangConfig!!!>), 
-        auth=basicauth, 
-        headers=<!!!REPLACEME with HTTP Header!!!>, 
-        verify=False
-        )
+
+    resp = requests.get(api_url, auth=basicauth, data=json.dumps(yangConfig),  headers=headers, verify=False)
 
     if(resp.status_code >= 200 and resp.status_code <= 299):
         print("STATUS OK: {}".format(resp.status_code))
-        return "<!!!REPLACEME with proper message!!!>"
+        return "Interface Loopback " +stdid " is create successfully!"
     else:
-        print('Error. Status Code: {}'.format(resp.status_code))
+        print('Cannot create: Interface loopback' +stdid".format(resp.status_code))
 
 
 def delete():
@@ -41,7 +55,7 @@ def delete():
         print("STATUS OK: {}".format(resp.status_code))
         return "<!!!REPLACEME with proper message!!!>"
     else:
-        print('Error. Status Code: {}'.format(resp.status_code))
+        print('Cannot delete: Interface loopback' +stdid .format(resp.status_code))
 
 
 def enable():
